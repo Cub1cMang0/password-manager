401e612ca7edaa053563a62a634e8a8301c17d93
Gabriel <gsotelosil14@gmail.com>
2025-04-20 16:56:11 -0700

    Added basic functionality in order to test with GUI .py file.

--- handle.py
+++ handle.py
@@ -1,4 +1,23 @@
-from main_work import manage
+from main_work import *
+
+def store(word: str, desc: str) -> None:
+    yes = manage(word, desc)
+    yes.setup()
+    yes.encrypt()
+    if os.path.exists(".helper"):
+        os.chdir(".helper")
+        yes.save_info()
+        os.chdir("..")
+    else:
+        hidden_dir(".helper")
+        os.chdir(".helper")
+        yes.save_info()
+        os.chdir("..")
+
+def master(passyword: str) -> None:
+    os.chdir(".helper")
+    set_master(passyword)
+    os.chdir("..")
 
 def main():
     return
