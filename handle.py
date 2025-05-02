2afbda23d47c690d5819c95fa48cd185fc9397fb
Gabriel <gsotelosil14@gmail.com>
2025-05-02 09:51:20 -0700

    Added more essential functions to be utililized by password-manager.py .

--- handle.py
+++ handle.py
@@ -5,6 +5,7 @@ def store(word: str, desc: str) -> None:
     yes.setup()
     yes.encrypt()
     if os.path.exists(".helper"):
+        grant_perms(".helper")
         os.chdir(".helper")
         yes.save_info()
         os.chdir("..")
@@ -13,23 +14,46 @@ def store(word: str, desc: str) -> None:
         os.chdir(".helper")
         yes.save_info()
         os.chdir("..")
+    rm_perms(".helper")
+
+def access():
+    grant_perms(".helper")
+    os.chdir(".helper")
+    with open("manager.json", "r") as file:
+        data = json.load(file)
+    os.chdir("..")
+    rm_perms(".helper")
+    return data
 
 def master(passyword: str) -> None:
+    grant_perms(".helper")
     os.chdir(".helper")
     set_master(passyword)
     os.chdir("..")
+    rm_perms(".helper")
 
 def first_time() -> bool:
+    grant_perms(".helper")
     os.chdir(".helper")
     if os.path.exists("master.json"):
         os.chdir("..")
+        rm_perms(".helper")
         return False
     else:
         os.chdir("..")
+        rm_perms(".helper")
         return True
+
+def present() -> bool:
+    grant_perms(".helper")
+    os.chdir(".helper")
+    exists = os.path.exists("manager.json")
+    os.chdir("..")
+    rm_perms(".helper")
+    return exists
 
 def main():
     return
 
 if __name__ == "__main__":
-    main()
+
